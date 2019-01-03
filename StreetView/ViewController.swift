e08662a210bdb4c8794e8068e0fd491c16a8ec30
Ilan Gallula <igg.gallu@gmail.com>
2019-01-03 22:52:01 +0200

    Changes - Ilan

--- StreetView/ViewController.swift
+++ StreetView/ViewController.swift
@@ -15,8 +15,13 @@ import GoogleMaps
 
 class ViewController: UIViewController, GMSMapViewDelegate {
     
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var streetView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
         let panoView = GMSPanoramaView(frame: .zero)
         self.view = panoView
@@ -26,7 +31,10 @@ class ViewController: UIViewController, GMSMapViewDelegate {
 //        panoView.moveNearCoordinate(CLLocationCoordinate2D(latitude: 32.064827, longitude: 34.774389))
         
 //        <iframe src="https://www.google.com/maps/embed?pb=!4v1544540417727!6m8!1m7!1sCAoSLEFGMVFpcE5kcFZnT0NMaW9OZndpRXBRZkdOa0RVaFlmSzZmWUxaRVBFa3Jr!2m2!1d32.06280684264727!2d34.7733187302947!3f169.5!4f0!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-        
+     
+//        let newButton = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 30))
+//        self.view.addSubview(newButton)
+//        newButton.backgroundColor = UIColor.blue
     }
 
     override func didReceiveMemoryWarning() {
